4ba98954bfc5b7cdf3c5ee1fd9831769ef557feb
universalle <30628568+universalle@users.noreply.github.com>
2020-01-14 01:55:14 +0200

    Create sender.py

---
+++ emulators_of_messages_for_EventHub/sender.py
@@ -0,0 +1,43 @@
+import sys
+import logging
+import datetime
+import time
+import os
+
+from azure.eventhub import EventHubClient, Sender, EventData
+
+logger = logging.getLogger("azure")
+
+# Address can be in either of these formats:
+# "amqps://<URL-encoded-SAS-policy>:<URL-encoded-SAS-key>@<namespace>.servicebus.windows.net/eventhub"
+# "amqps://<namespace>.servicebus.windows.net/<eventhub>"
+# SAS policy and key are not required if they are encoded in the URL
+
+ADDRESS = "amqps://iventhub.servicebus.windows.net/cnh-bdgs-dev"
+USER = "RootManageSharedAccessKey"
+KEY = "RUJ5QieOO7YuuvS9sT06SCB1ScQxJU36lU8Bl6gaWGI="
+
+try:
+    if not ADDRESS:
+        raise ValueError("No EventHubs URL supplied.")
+
+    # Create Event Hubs client
+    client = EventHubClient(ADDRESS, debug=False, username=USER, password=KEY)
+    sender = client.add_sender(partition="0")
+    client.run()
+    try:
+        start_time = time.time()
+        for i in range(100):
+            print("Sending message: {}".format(i))
+            message = "Message {}".format(i)
+            sender.send(EventData(message))
+    except:
+        raise
+    finally:
+        end_time = time.time()
+        client.stop()
+        run_time = end_time - start_time
+        logger.info("Runtime: {} seconds".format(run_time))
+
+except KeyboardInterrupt:
+    pass
